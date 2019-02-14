dc64cc16a9ae39adb88336564efc6d7c930972ff
Pine Wu <octref@gmail.com>
2019-02-13 23:52:32 -0800

    Add vue-eslint-parser

---
+++ website/src/parsers/vue/eslint-vue.js
@@ -0,0 +1,33 @@
+import pkg from 'vue-eslint-parser/package.json';
+
+import defaultParserInterface from '../utils/defaultParserInterface';
+
+export default {
+  ...defaultParserInterface,
+
+  id: 'vue',
+  displayName: 'Vue',
+  version: pkg.version,
+  homepage: pkg.homepage,
+
+  locationProps: new Set(['loc', 'range']),
+
+  loadParser(callback) {
+    require(['vue-eslint-parser'], (vep) => callback(vep.parse))
+  },
+
+  opensByDefault(node) {
+    return true;
+  },
+
+  parse(parseVue, code) {
+    return parseVue(code).templateBody;
+  },
+
+  nodeToRange({ range }) {
+    if (!range) return;
+    return range;
+  },
+
+  _ignoredProperties: new Set(['loc', 'start', 'end', 'range', 'parent', 'rawName', 'namespace', 'type', 'tokens', 'comments', 'errors']),
+};
---
+++ website/src/parsers/vue/index.js
@@ -0,0 +1,6 @@
+import 'codemirror/mode/vue/vue';
+
+export const id = 'vue';
+export const displayName = 'Vue';
+export const mimeTypes = ['text/html'];
+export const fileExtension = 'vue';
